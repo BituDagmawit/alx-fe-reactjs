4149c5a213a6dcda9a8d7893093cf4148c5190ec
talaksew agegnehu <talaksewmisganaw1@gmail.com>
2025-11-30 23:18:30 +0300

    react with css

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,11 @@
+import UserProfile from "./components/UserProfile";
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-white w-full">
+      <UserProfile />
+    </div>
+  );
+}
+
+export default App;
