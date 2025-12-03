5fa653279e7a623d76afa637a7eac243348de0ad
Bitanya Mulugeta <Bitanyadagmawit16@gmail.com>
2025-12-03 19:16:17 +0300

    responsive error fix

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -10,7 +10,7 @@ export default function HomePage() {
   }, []);
 
   return (
-    <div className="p-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+    <div className="p-6 grid grid-cols-1 md:p-9 sm:grid-cols-2 lg:grid-cols-3 gap-6">
       {recipes.map(recipe => (
         <div
           key={recipe.id}
