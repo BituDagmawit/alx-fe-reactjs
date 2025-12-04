24551f799214a3a3feeea9551521b6749402e3c7
Bitanya Mulugeta <Bitanyadagmawit16@gmail.com>
2025-12-04 13:27:29 +0300

    fix 7

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,15 +1,30 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeList from './components/RecipeList';
+import RecipeDetails from './components/RecipeDetails';
 import SearchBar from './components/SearchBar';
+import FavoritesList from './components/FavoritesList';
 
-function App() {
+function Home() {
   return (
     <div style={{ padding: '20px' }}>
       <h1>Recipe Sharing App</h1>
       <SearchBar />
       <AddRecipeForm />
       <RecipeList />
+      <FavoritesList />
     </div>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/recipes/:id" element={<RecipeDetails />} />
+      </Routes>
+    </Router>
   );
 }
 
