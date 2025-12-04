368c5e843ae512f3a4a2fa351efc22263d8a8b45
Bitanya Mulugeta <Bitanyadagmawit16@gmail.com>
2025-12-04 11:17:41 +0300

    context error 3

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -5,7 +5,7 @@ import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
 import Counter from './components/Counter';
 
-import UserContext from './context/UserContext';
+import UserContext from './UserContext';
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
