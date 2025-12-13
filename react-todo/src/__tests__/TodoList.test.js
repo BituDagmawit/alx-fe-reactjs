576c04046afe796c3f42ad2296f98cf9643df1e0
Bitanya Mulugeta <talaksewmisganaw1@gmail.com>
2025-12-13 15:50:36 +0300

    fix on implementation of test 4

--- react-todo/src/__tests__/TodoList.test.js
+++ react-todo/src/__tests__/TodoList.test.js
@@ -2,32 +2,39 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import TodoList from '../components/TodoList';
 
 
+describe('TodoList Component', () => {
 beforeEach(() => render(<TodoList />));
 
 
-test('renders initial todos', () => {
+test('renders initial demo todos', () => {
 expect(screen.getByText('Learn React')).toBeInTheDocument();
 expect(screen.getByText('Write tests')).toBeInTheDocument();
 });
 
 
-test('adds a new todo', () => {
-fireEvent.change(screen.getByPlaceholderText('Add todo'), {
-target: { value: 'New Todo' },
-});
-fireEvent.click(screen.getByText('Add'));
-expect(screen.getByText('New Todo')).toBeInTheDocument();
+test('can add a new todo', () => {
+const input = screen.getByPlaceholderText('Add todo');
+const addButton = screen.getByText('Add');
+
+
+fireEvent.change(input, { target: { value: 'ALX Todo' } });
+fireEvent.click(addButton);
+
+
+expect(screen.getByText('ALX Todo')).toBeInTheDocument();
 });
 
 
-test('toggles todo completion', () => {
+test('can toggle todo completion', () => {
 const todo = screen.getByText('Learn React');
 fireEvent.click(todo);
 expect(todo).toHaveStyle('text-decoration: line-through');
 });
 
 
-test('deletes a todo', () => {
-fireEvent.click(screen.getAllByText('Delete')[0]);
+test('can delete a todo', () => {
+const deleteButtons = screen.getAllByText('Delete');
+fireEvent.click(deleteButtons[0]);
 expect(screen.queryByText('Learn React')).not.toBeInTheDocument();
+});
 });
