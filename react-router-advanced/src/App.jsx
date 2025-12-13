663b5c45556e420025a38293dd4000b10cb5b6ec
Bitanya Mulugeta <Bitanyadagmawit16@gmail.com>
2025-12-13 11:48:56 +0300

    fix 3

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Profile from "./Profile";
+import BlogPost from "./BlogPost";              // REQUIRED
 import ProtectedRoute from "./components/ProtectedRoute";
 
 export default function App() {
@@ -9,7 +10,10 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
 
-        {/* PROTECTED ROUTE */}
+        {/* REQUIRED dynamic route */}
+        <Route path="/blog/:id" element={<BlogPost />} />
+
+        {/* REQUIRED protected route */}
         <Route
           path="/profile"
           element={
