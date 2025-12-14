08d25c206f93253a697b6c30a425185b86497028
Bitanya Mulugeta <talaksewmisganaw1@gmail.com>
2025-12-14 23:05:33 +0300

    lst

--- react-todo/src/main.jsx
+++ react-todo/src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import TodoList from "./components/TodoList";
+import "./index.css"; // optional, if you have global styles
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
